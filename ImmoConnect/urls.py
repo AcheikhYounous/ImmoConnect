6dfe5330fa562323f3506447ab9e2bbce3516882
Mahamat-Saleh-Anour <madjidejan4@gmail.com>
2025-10-31 04:16:51 +0100

    Le version 0.0.1 pour se lancer en attendant que Mr ISSA repond

--- ImmoConnect/urls.py
+++ ImmoConnect/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static # Pour servir les fichiers médias en développement
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # Pour servir les fichiers statiques en développement
+from . import settings # Importer les paramètres du projet
+from django.conf.urls import handler404, handler403, handler500 # Importer les gestionnaires d'erreurs personnalisés tel que erreur 404, 403, 500
+# from users.views import custom_403_view, custom_404_view, custom_500_view # Importer les vues personnalisées pour les erreurs
+
+# handler404 = custom_404_view
+# handler403 = custom_403_view
+# handler500 = custom_500_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('users.urls', namespace='users')),  # URLs pour l'application des utilisateurs
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
